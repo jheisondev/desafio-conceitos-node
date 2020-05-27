9c8c908d3d16c0f4cfc29863f4bdaf27f5f8e31e
Jheison Maciel Ines <jheisonmaciel@hotmail.com>
2020-05-27 14:49:54 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,79 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  
+
+  // const techsArray = techs.split(/\s*,\s*/);
+
+  const repo = { 
+    id: uuid(), 
+    title, 
+    url, 
+    techs, 
+    likes: 0, 
+  };
+
+  repositories.push(repo);
+  return response.json(repo);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repo not find' })
+  }
+
+  // const techsArray = techs.split(/\s*,\s*/);
+
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repoIndex].likes,
+  }
+
+  repositories[repoIndex] = repo;
+  console.log(repo);
+
+  return response.json(repo);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex >= 0) {
+    repositories.splice(repoIndex, 1);
+  } else {
+    return response.status(400).json({ error: 'Repo not find' })
+  }
+  
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repo not find' })
+  }
+  
+  repositories[repoIndex].likes++;
+
+  return response.json(repositories[repoIndex]);
 });
 
 module.exports = app;
